5ae30b74a1f2946ed6216d65cbec3b1256f5308a
umair.khan <umair.khan>
2018-09-30 15:20:19 +0400

    initial commit

---
+++ app/src/test/java/com/dubizzle/playground/ExampleUnitTest.kt
@@ -0,0 +1,19 @@
+package com.dubizzle.playground
+
+import com.dubizzle.dbzanalytics.common.constant.EventNames
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        val x = EventNames.ADD_TO_FAVORITES;
+        println(x)
+    }
+}
